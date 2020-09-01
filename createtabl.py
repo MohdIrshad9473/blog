7bfee317f635cdf2755647b504568678bf3344bc
MohdIrshad9473 <irshad9473@gmail.com>
2020-09-01 16:11:36 +0530

    initial

--- createtabl.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, render_template
-from datetime import datetime
-app = Flask(__name__)   #web site app
-@app.route(‘/hello’)
-def hello_world():
-   return ‘hello world’
-
-)
